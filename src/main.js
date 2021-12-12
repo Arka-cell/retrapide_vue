85d1a7a7dff59f7c54ec446e8670db998ca67727
Ahmane Samir <samirahmane.trading@gmail.com>
2021-12-12 09:46:08 +0100

    config: configured main.js

--- src/main.js
+++ src/main.js
@@ -1,4 +1,28 @@
-import { createApp } from 'vue'
+import Vue from "vue";
 import App from './App.vue'
+import firebase from "firebase/app";
+import "@/axios-firebase";
+import router from "./router";
+import store from "./store";
+import Buefy from "buefy";
+import "buefy/dist/buefy.css";
 
-createApp(App).mount('#app')
+Vue.config.productionTip = false;
+Vue.use(Buefy);
+const firebaseConfig = {
+    apiKey: "AIzaSyD3Lk6zKTSlwiORxlyXGUGsXf4Y0BJNnkw",
+    authDomain: "retrapide-f9e63.firebaseapp.com",
+    projectId: "retrapide-f9e63",
+    storageBucket: "retrapide-f9e63.appspot.com",
+    messagingSenderId: "1072771612057",
+    appId: "1:1072771612057:web:ce275e87a808b415eb2708",
+    measurementId: "${config.measurementId}"
+  };
+
+firebase.initializeApp(firebaseConfig);
+
+new Vue({
+    router,
+    store,
+    render: (h) => h(App),
+  }).$mount("#app");
