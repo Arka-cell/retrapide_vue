9681effb4cadd4fe057f1b4a649ae1e6c1626cae
Ahmane Samir <samirahmane.trading@gmail.com>
2021-12-22 20:41:31 +0100

    feat: added NavBar to app

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,61 @@
 <template>
   <div id="app">
-    <div v-if="user" id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link> |
-      <router-link :to="{ name: 'Profile' }">Profile</router-link> |
-    </div>
-    <div v-else id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link> |
-      <router-link :to="{ name: 'Login' }">Login</router-link> |
-    </div>
+    <nav class="navbar py-4">
+      <div class="container">
+        <div class="navbar-brand is-centered">
+          <router-link class="navbar-item" to="/">
+            <img
+              class="image is-flex is-centered"
+              src="./assets/suitcase.png"
+            />
+          </router-link>
+          <a
+            class="navbar-burger"
+            role="button"
+            aria-label="menu"
+            aria-expanded="false"
+          >
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+          </a>
+        </div>
+        <div class="navbar-menu">
+          <div class="navbar-end">
+            <router-link class="navbar-item" to="/about">About</router-link
+            ><router-link class="navbar-item" to="/companies"
+              >Companies</router-link
+            ><router-link class="navbar-item" to="/services"
+              >Services</router-link
+            ><router-link class="navbar-item" to="/chat">Chat</router-link>
+          </div>
+          <div class="navbar-item">
+            <div id="login" v-if="user" class="buttons">
+              <router-link class="button" :to="{ name: 'Profile' }"
+                >Profile</router-link
+              >
+              <button class="button is-danger" @click="signOut()">Logout</button>
+            </div>
+            <div id="logout" v-else class="buttons">
+              <router-link class="button" :to="{ name: 'Login' }"
+                >Login as a user</router-link
+              >
+              <router-link class="button" :to="{ name: 'Login' }"
+                >Login as a company</router-link
+              >
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
+
     <router-view />
   </div>
 </template>
 
 <script>
+import firebase from "firebase/app";
+
 export default {
   data() {
     return {
@@ -23,6 +64,13 @@ export default {
   },
   async mounted() {
     this.user = await this.$store.state.user;
+  },
+  methods: {
+    signOut() {
+      firebase.auth().signOut();
+      this.user = null;
+      this.$store.dispatch("updateUser", this.user);
+    },
   },
 };
 </script>
