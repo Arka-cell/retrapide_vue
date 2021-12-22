f74c29c07a062297839dbba77e452d6ef53ed131
Ahmane Samir <samirahmane.trading@gmail.com>
2021-12-22 10:41:08 +0100

    feat: added rendering condition based on whether user is auth or not

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,32 @@
 <template>
   <div id="app">
-    <div id="nav">
+    <div v-if="user" id="nav">
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link> |
+      <router-link :to="{ name: 'Profile' }">Profile</router-link> |
+    </div>
+    <div v-else id="nav">
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link> |
       <router-link :to="{ name: 'Login' }">Login</router-link> |
-      <router-link :to="{ name: 'Profile' }">Profile</router-link> |
     </div>
     <router-view />
   </div>
 </template>
+
+<script>
+export default {
+  data() {
+    return {
+      user: null
+    }
+  },
+  async mounted() {
+    this.user = await this.$store.state.user
+    console.log(this.user)
+  },
+  computed: {
+    
+  }
+}
+</script>
