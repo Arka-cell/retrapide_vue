16735ae26b28d43c8bad8f7c07f537acdfdaa0a3
Ahmane Samir <samirahmane.trading@gmail.com>
2021-12-22 10:21:36 +0100

    feat: added Vuex store objects

--- src/store/index.js
+++ src/store/index.js
@@ -4,8 +4,26 @@ import Vuex from "vuex";
 Vue.use(Vuex);
 
 export default new Vuex.Store({
-  state: {},
-  mutations: {},
-  actions: {},
-  modules: {},
+  state: {
+    user: null
+  },
+  mutations: {
+    SET_USER(state, user) {
+      state.user = user
+    }
+  },
+  actions: {
+    updateUser({commit}, user) {
+      if (user) {
+        commit('SET_USER', user)
+      } else {
+        commit('SET_USER', null)
+      }
+    }
+  },
+  getters: {
+    user(state){
+      return state.user
+    }
+  },
 });
