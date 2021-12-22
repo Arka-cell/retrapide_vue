31cae0fcd2b3bde8fe4e77b8ace67b998409948a
Ahmane Samir <samirahmane.trading@gmail.com>
2021-12-22 20:42:22 +0100

    feat: profile view conotains personalinfosection component now

--- src/views/ProfileView.vue
+++ src/views/ProfileView.vue
@@ -1,25 +1,16 @@
 <template>
   <div id="profile-view" class="container">
-    <div class="columns is-multiline is-centered">
-      <PersonalInfoSection />
-      <CVSection />
-
-      <section class="section column is-8">
-        <h1 class="title">Se déconnecter</h1>
-        <b-button @click="signOut">Se déconnecter</b-button>
-      </section>
-    </div>
+    <PersonalInfoSection />
   </div>
 </template>
 
 <script>
 import PersonalInfoSection from "@/components/PersonalInfoSection.vue";
-import CVSection from "@/components/CVSection.vue";
 import firebase from "firebase/app";
 
 export default {
   name: "ProfileView",
-  components: { PersonalInfoSection, CVSection },
+  components: { PersonalInfoSection },
   methods: {
     signOut() {
       firebase.auth().signOut();
