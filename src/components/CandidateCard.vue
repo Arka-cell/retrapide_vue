b2b7d30c1965c34bbd4f97853690ee4a6e44ef48
Ahmane Samir <samirahmane.trading@gmail.com>
2022-01-21 17:12:52 +0100

    feat: created Candidate Card

---
+++ src/components/CandidateCard.vue
@@ -0,0 +1,76 @@
+<template>
+  <div>
+    <div class="card">
+      <div class="card-content">
+        <div class="media">
+          <div class="media-left">
+            <figure class="image is-96x96">
+              <img :src="this.profile_img" class="is-rounded" />
+            </figure>
+          </div>
+          <div class="media-content">
+            <p class="title is-4">
+              {{ candidate.first_name }} {{ candidate.last_name }}
+            </p>
+            <p class="subtitle is-6">{{ candidate.email }}</p>
+          </div>
+        </div>
+
+        <div class="content">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Phasellus nec
+          iaculis mauris. <a>@bulmaio</a>. <a href="#">#css</a>
+          <a href="#">#responsive</a>
+          <br />
+          <time datetime="2016-1-1">11:09 PM - 1 Jan 2016</time>
+        </div>
+        <div class="buttons is-right">
+          <button class="button is-right is-success is-light" @click="getResume()">See CV</button>
+        </div>
+      </div>
+    </div>
+    {{ candidate }}
+    <div v-if="this.resume_src" class="box">
+      <iframe :src="this.resume_src"></iframe>
+    </div>
+  </div>
+</template>
+
+<script>
+import firebase from "firebase";
+
+export default {
+  name: "CandidateCard",
+  props: {
+    content: { type: Object, required: true },
+  },
+  data() {
+    return {
+      candidate: this.content,
+      profile_img: "",
+      resume_src: "",
+    };
+  },
+  mounted() {
+    this.getImage();
+  },
+  methods: {
+    async getImage() {
+      console.log(await this.$store.state.user);
+      const storage = firebase.storage();
+      const storageRef = storage.ref();
+      const imageRef = storageRef.child("img/" + this.candidate.user_id);
+      imageRef.getDownloadURL().then((url) => {
+        this.profile_img = url;
+      });
+    },
+    async getResume() {
+      const storage = firebase.storage();
+      var storageRef = storage.ref();
+      const resumeRef = storageRef.child(`${this.candidate.user_uid}/${this.candidate.user_uid}.pdf`);
+      resumeRef.getDownloadURL().then((url) => {
+        this.resume_src = url + "#view=Fit";
+      });
+    },
+  },
+};
+</script>
