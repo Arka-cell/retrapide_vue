60cd11b6adb62bd6474b24c7b005fa978498f330
Ahmane Samir <samirahmane.trading@gmail.com>
2022-01-24 13:06:10 +0100

    feat: catching error if token is null

--- src/axios-firebase.js
+++ src/axios-firebase.js
@@ -1,10 +1,15 @@
 import axios from "axios";
 import firebase from "firebase/app";
+import "firebase"
 
 axios.defaults.baseURL = process.env.VUE_APP_API_URL;
 axios.interceptors.request.use(async (config) => {
-  let token = await firebase.auth().currentUser.getIdToken(true);
+  try {
+    let token = await firebase.auth().currentUser.getIdToken(true);
 
-  config.headers.Authorization = `Bearer ${token}`;
-  return config;
+    config.headers.Authorization = `Bearer ${token}`;
+    return config;
+  } catch {
+    return config;
+  }
 });
