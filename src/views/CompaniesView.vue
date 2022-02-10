4b5ffb246ed204e056165e77a2ff523172895378
Samir Ahmane <samirahmane.trading@gmail.com>
2022-02-10 09:18:18 +0100

    feat: created CompaniesView

---
+++ src/views/CompaniesView.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="columns is-centered">
+        <div class="is-four-fifths">
+            <CompanyCard />
+        </div>
+    </div>
+</template>
+<script>
+import CompanyCard from "@/components/CompanyCard"
+export default {
+    components: {
+        CompanyCard
+    }
+}
+</script>
