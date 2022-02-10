f38f402f5f11485b8bda9691097e69234bff712e
Samir Ahmane <samirahmane.trading@gmail.com>
2022-02-10 09:56:17 +0100

    feat: finished CompaniesView.vue features

--- src/views/CompaniesView.vue
+++ src/views/CompaniesView.vue
@@ -1,15 +1,38 @@
 <template>
-    <div class="columns is-centered">
-        <div class="is-four-fifths">
-            <CompanyCard />
-        </div>
+  <div class="box has-text-centered">
+    <div class="columns is-multiline is-centered">
+      <CompanyCard
+        v-for="company in companies"
+        :key="company.id"
+        :content="company"
+      />
     </div>
+  </div>
 </template>
 <script>
-import CompanyCard from "@/components/CompanyCard"
+import CompanyCard from "@/components/CompanyCard";
+import axios from "axios";
+
 export default {
-    components: {
-        CompanyCard
-    }
-}
+  components: {
+    CompanyCard,
+  },
+  data() {
+    return {
+      page: 1,
+      companies: [],
+      n_pages: 0,
+    };
+  },
+  methods: {
+    async getCompanies() {
+      const response = await axios.get(`/companies/${this.page}/`);
+      this.companies = response.data.companies;
+      this.n_pages = response.data.n_pages;
+    },
+  },
+  mounted() {
+    this.getCompanies();
+  },
+};
 </script>
